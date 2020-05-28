451007a958d6315ae00dfead1b043167d2303302
Toyin Hawau Olamide <hawau.olamide.th@gmail.com>
2020-05-28 22:48:09 +0100

    changes

--- src/routers/user.js
+++ src/routers/user.js
@@ -11,7 +11,7 @@ router.post('/users', async (req,res) =>{
         await user.save()
         sendWelcomeEmail(user.email, user.name)
         const token = await user.generateAuthToken()
-        res.status(201).send({user })
+        res.status(201).send({user, token })
     }
     catch (err) {
         res.status(400).send(err)
